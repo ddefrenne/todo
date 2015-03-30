80d8eef839521ec4de306e1826e52e807c5ece14
Dimitri De Frenne <ddefrenne@gmail.com>
2015-03-30 17:37:52 +0200

    Add endpoint for adding a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,18 @@ class TasksController < ActionController::Base
     render json: @tasks
   end
 
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:description)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'assets/index'
 
   get '/tasks' => 'tasks#index'
+  post '/tasks' => 'tasks#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
