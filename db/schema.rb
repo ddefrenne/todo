c29e2001ce18defba754d991517f4549bf76cc5b
Dimitri De Frenne <ddefrenne@gmail.com>
2015-04-10 19:31:06 +0200

    Rails: tasks have to be marked as complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324152913) do
+ActiveRecord::Schema.define(version: 20150410172827) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "completed",   default: false
   end
 
 end
